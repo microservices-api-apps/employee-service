4d95005432492bdcbef4f90a1c90d91e933e0f05
Anand <arockiaanandraj@gmail.com>
2019-11-19 22:57:55 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into employee(emp_id,first_name,last_name,date_of_joining) values (1000, 'AAA', 'BBB', sysdate());
-insert into employee(emp_id,first_name,last_name,date_of_joining) values (1001, 'CCCC', 'FFF', sysdate());
-insert into employee(emp_id,first_name,last_name,date_of_joining) values (1002, 'GGGG', 'EEE', sysdate());
+insert into employee(emp_id,first_name,last_name,date_of_joining) values (1000, 'John', 'Doe', sysdate());
+insert into employee(emp_id,first_name,last_name,date_of_joining) values (1001, 'Jane', 'Doe', sysdate());
+insert into employee(emp_id,first_name,last_name,date_of_joining) values (1002, 'Will', 'Smith', sysdate());
